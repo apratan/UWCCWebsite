f4add1e6fdac92857b6cca3730dd92ce94b1be1b
ratanap <ratanap@uw.edu>
2020-07-17 20:37:50 -0700

    Added dummy code for playing aroundwith pages

--- src/desktop/Desktop.js
+++ src/desktop/Desktop.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
 
 function App() {
+  const [showTextField, setShowTextField] = useState(true);
+
   return (
     <div className="App">
       <Button
         variant="contained"
-        color="primary"
-        onClick={() => alert('Stop touching me!')}
+        color="secondary"
+        onClick={() => {
+          setShowTextField(!showTextField);
+        }}
       >
-        Hello, Browser!
+        Toggle Input
       </Button>
+      <br />
+      {showTextField && (
+        <TextField variant="outlined" label="Can you see me?" />
+      )}
     </div>
   );
 }
