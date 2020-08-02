d1b4cdb8489759679a034c1fe23e49517322fc35
somchai joemjumroon <Prachatornjoe@gmail.com>
2020-08-02 00:52:58 -0700

    Edited Footer

--- src/desktop/Desktop.js
+++ src/desktop/Desktop.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from "react";
+// import Button from '@material-ui/core/Button';*
+import "./footer.scss";
+import Facebook from "./facebook.png";
+import Instagram from "./Instagram.jpg";
+import Twitter from "./twitter.png";
+// import LinkedIn from './linkedin.png';
+import Pornhub from "./Pornhub.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => alert('Stop touching me!')}
-      >
-        Hello, Browser!
-      </Button>
+      {/* <Button
+         variant="contained"
+         color="primary"
+         onClick={() => alert('Stop touching me!')}
+       >
+         Hello, Browser!
+       </Button> */}
+      <footer id="footer">
+        <div id="ThankYou">
+          <h2>Thank you for Sharing!</h2>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div id="logos">
+          <a href="https://www.facebook.com/groups/1251744728327172">
+            <img className="LogoPic" src={Facebook} alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com/uwcomedyclub/">
+            <img className="LogoPic" src={Instagram} alt="Instagram" />
+          </a>
+          <a href="https://twitter.com/uwcomedyclub">
+            <img className="LogoPic" src={Twitter} alt="Twitter" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+            {/* <img className="LogoPic" src={LinkedIn} alt="LinkedIn" /> */}
+            <img className="LogoPic" src={Pornhub} alt="Pornhub For Real!" />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
